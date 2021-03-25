1175b81070719a0152a4c86370b5a7855499295d
Ante Sorić <asoric00@fesb.hr>
2021-03-25 19:58:54 +0100

    sdawdsa

--- AS_vjezba_1/Program.cs
+++ AS_vjezba_1/Program.cs
@@ -10,7 +10,7 @@ namespace AS_vjezba_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!!");
+            Console.WriteLine("Hello World!!");
         }
     }
 }
